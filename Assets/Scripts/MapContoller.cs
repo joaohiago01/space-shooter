9c7104b2d61cb4137108a45a6ab8e28a363efb11
unknown <inathan.moreira.10@gmail.com>
2020-07-18 16:36:50 -0300

    Add Map Scene

---
+++ Assets/Scripts/MapContoller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MapContoller : MonoBehaviour
+{
+
+    void Start() {
+   
+    }
+
+    void Update() {
+        
+    }
+
+    public void startPhase(string phase) {
+        SceneManager.LoadScene(phase);
+    }
+
+    public void backMenuScene() {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+}
---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShopController : MonoBehaviour
+{
+    void Start() {
+        
+
+
+
+    }
+
+    void Update() {
+        
+    }
+
+
+    public void ExitShop() {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+}
